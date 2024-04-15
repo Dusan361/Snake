c689217286dc5cabf92f78f0a490562edb693dc3
Dusan Krtinic <dule147@gmail.com>
2024-04-15 12:44:35 +0200

    Game Over

--- snake.js
+++ snake.js
@@ -19,6 +19,7 @@ let snakeBody = [];
 let foodX;
 let foodY; 
 
+let gameOver = false;
 
 window.onload = function (){
     board = document.getElementById("board");
@@ -34,6 +35,9 @@ window.onload = function (){
 }
 
 function update(){
+    if(gameOver){
+        return;
+    }
     context.fillStyle = "black";
     context.fillRect(0,0,board.width, board.height);
 
@@ -45,13 +49,37 @@ function update(){
        placeFood(); 
     }
 
+    for (let i = snakeBody.length-1; i>0; i--){
+        snakeBody[i] = snakeBody[i-1];
+    }
+
+    if(snakeBody.length){
+        snakeBody[0] = [snakeX,snakeY];
+    }
+
     context.fillStyle = "lime";
     snakeX += velocityX * blockSize;
     snakeY += velocityY * blockSize;
     context.fillRect(snakeX,snakeY,blockSize,blockSize);
-    for (let i=0; < snakeBody.length; i++){
+    for (let i=0; i < snakeBody.length; i++){
         context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize,blockSize);
     }
+
+    // game over conditions
+    if(snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize){
+        gameOver = true;
+        alert("Game Over");
+
+    }
+
+    for(let i =0; i < snakeBody.length; i++){
+        if(snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]){
+            gameOver = true;
+            alert("Game Over");
+        }
+    }
+
+
 
 
 }
